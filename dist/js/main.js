32f25d30f526d73a17711c944f3f824c349eccbc
yirou <yirou@ualberta.ca>
2023-03-17 15:53:28 -0600

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@ const menu=document.querySelector('.menu');
 const menuNav=document.querySelector('.menu-nav');
 const menuBranding=document.querySelector('.menu-branding');
 const navItems=document.querySelectorAll(
-    '.nav-items'
+    '.nav-item'
 );
 
 let showMenu=false;
@@ -19,10 +19,10 @@ function toggleMenu(){
     }
         
         else{
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('show');
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
         navItems.forEach(item=>item.classList.remove('show'));
         showMenu=false;
 
